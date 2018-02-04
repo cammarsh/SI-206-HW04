d66f468481319e0a871f84d8f86c499c782d28b8
Erin Saada <esaada@umich.edu>
2018-02-04 16:24:37 -0500

    added questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,5 @@
-
+import random
 def questions():
     return input("What is your question")
-
-
-
-
+questions = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply haze try again", "Ask again later", "Better not tell you now", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+print(random.choice(questions))
